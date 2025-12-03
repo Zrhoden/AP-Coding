7bfd75d423c5619d0a1087b9730234153a5410fb
Zrhoden <zrhoden157@boyslatin.net>
2025-12-03 16:01:24 +0000

    Visulization.py

--- unit 4/visualization.py
+++ unit 4/visualization.py
@@ -9,6 +9,6 @@ plot_position_stat_bar(2024, "RB", "rushing_yards", save_path="rb_rushing_2024.p
 #2
 plot_player_stat_by_week(2024, 'Jalen', 'Hurts', 'passing_yards', save_path='jalen_hurts_passing_yards_2024.png')
 
-'Jalen hurts has actually declined sincer 2022 where he had 5 games with 300 yards or more,'
+'Jalen hurts has actually declined since 2022 where he had 5 games with 300 yards or more,'
 ' he followed up 2022 with a good year in 2023 where he also had 5 games with 300 or more yards,'
 ' but in 2024 he oinly had one game with 300 yards or more'
