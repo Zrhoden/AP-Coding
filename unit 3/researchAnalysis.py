850e9b662778e0b942122ee78a3464539c707f77
Zrhoden <zrhoden157@boyslatin.net>
2025-12-03 16:00:12 +0000

    vizualization.py

---
+++ unit 3/researchAnalysis.py
@@ -0,0 +1,12 @@
+from helperLogic import get_player_stats_by_name, plot_weekly_player_stats,plot_player_stat, get_team_records, get_advanced_team_records, get_position_columns, get_season_Results_By_team , weeklyPlayerStats
+import matplotlib.pyplot as plt
+
+#1 which division had the best defense in terms of yards allowed
+
+#2 which reciver had the most targets vs receptions in 2024
+'Jamaar Chase'
+#3 does time of possesion strongly correlate with wins in 2024
+'While it has some correlation, its not strong. Sometimes time having the ball for less time is a good thing, and sometimes its a bad thing'
+
+
+    
---
+++ unit 4/visualization.py
@@ -0,0 +1,14 @@
+from helperFunctions import plot_position_stat_bar, get_player_stats, dataframe_to_png, export_player_season_png,plot_player_stat_by_week
+import matplotlib.pyplot as plt
+
+#1      
+plot_position_stat_bar(2024, "RB", "rushing_yards", save_path="rb_rushing_2024.png", top_n=25)
+
+'The leading rusher over this period is saquon barkley with around 4900'
+
+#2
+plot_player_stat_by_week(2024, 'Jalen', 'Hurts', 'passing_yards', save_path='jalen_hurts_passing_yards_2024.png')
+
+'Jalen hurts has actually declined sincer 2022 where he had 5 games with 300 yards or more,'
+' he followed up 2022 with a good year in 2023 where he also had 5 games with 300 or more yards,'
+' but in 2024 he oinly had one game with 300 yards or more'
