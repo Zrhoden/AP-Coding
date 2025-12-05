04345197204c1b5bda560c7656b140fecaa71279
Zrhoden <zrhoden157@boyslatin.net>
2025-12-05 15:52:37 +0000

    Washed

---
+++ unit 4/washed.py
@@ -0,0 +1,20 @@
+from helperFunctions import plot_position_stat_bar, get_player_stats, dataframe_to_png, export_player_season_png,plot_player_stat_by_week
+import matplotlib.pyplot as plt
+
+
+
+
+plot_player_stat_by_week(2016, 'Russell', 'Wilson', 'passing_yards', save_path='russell_wilson_passing_yards_2016.png')
+
+
+
+#1
+'If a player is washed it means that they are no longer as good as they used to be. '
+'You can track things like passing and rushing yards, touchdowns, and completion '
+'percentage to track if quarterbacks are washed.'
+
+#2
+
+'A player that I believe is washed is Russell Wilson. All through the 2010s Wilson was a perinial mvp canidate, but he saw a massive dip in production after 2020. '
+'In 2020 Wilson threw for 4,212 yards, and in the following year of 2021 he threw for 3,113 yards which is 1099 yard drop. '
+'After 2021 wilson has stayed throwing around 3000 yards, but he has been benched more than once since then, and also has had multiple losing records and playoff failures.'
